d43a9a2ad7c2df4bd211473e264fd2cac08d307e
sensactive <sensor14@list.ru>
2019-07-24 12:36:28 +0300

    added test and logging(hw5_done)

--- server/__main__.py
+++ server/__main__.py
@@ -1,7 +1,7 @@
 from socket import socket
 from argparse import ArgumentParser
 import json
-
+import logging
 from protocol import validate_request, make_response
 from resolvers import resolve
 
@@ -18,7 +18,7 @@ parser.add_argument(
 args = parser.parse_args()
 
 default_config = {
-    'host': '',
+    'host': '127.0.0.1',
     'port': 7777,
     'buffersize': 1024
 }
@@ -30,17 +30,28 @@ if args.port:
 
 host, port = (default_config.get('host'), default_config.get('port'))
 
+logger = logging.getLogger('main')
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+handler = logging.FileHandler('main.log')
+handler.setFormatter(formatter)
+handler.setLevel(logging.DEBUG)
+
+logger.addHandler(handler)
+
 try:
 
     sock = socket()
     sock.bind((host, port,))
     sock.listen(5)
 
-    print(f'Server was started with {host}:{port}')
+    logger.info(f'Server was started with {host}:{port}')
 
     while True:
         client, address = sock.accept()
-        print(f'Client was connected with {address[0]}:{address[1]}')
+        logger.info(f'Clietn was connected with {address[0]}:{address[1]}')
         b_request = client.recv(default_config.get('buffersize'))
         request = json.loads(b_request.decode())
 
@@ -49,16 +60,16 @@ try:
             controller = resolve(action_name)
             if controller:
                 try:
-                    print(f'Controller {action_name} resolved with request: {request}')
+                    logger.debug(f'Controller {action_name} resolved with request: {request}')
                     response = controller(request)
                 except Exception as err:
-                    print(f'Controller {action_name} error: {err}')
+                    logger.critical(f'Controller {action_name} error: {err}')
                     response = make_response(request, 500, 'Internal server error')
             else:
-                print(f'Controller {action_name} not found')
+                logger.error(f'Controller {action_name} not found')
                 response = make_response(request, 404, f'Action with name {action_name} not supported')
         else:
-            print(f'Controller wrong request: {request}')
+            logger.error(f'Controller wrong request: {request}')
             response = make_response(request, 400, 'wrong request format')
 
         client.send(
@@ -68,4 +79,4 @@ try:
 
 
 except KeyboardInterrupt:
-    print(' Server shutdown')
+    logger.info('Server shutdown')
