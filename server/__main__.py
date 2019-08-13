99fa71959d1f8db50d4f30e6c767d394bda56c33
sensactive <sensor14@list.ru>
2019-08-13 09:59:30 +0300

    oop server

--- server/__main__.py
+++ server/__main__.py
@@ -1,5 +1,6 @@
 from socket import socket
 from argparse import ArgumentParser
+from app import Application
 import select
 import json
 import logging
@@ -9,21 +10,7 @@ from protocol import validate_request, make_response
 from resolvers import resolve
 
 
-def read(sock, connections, requests, buffersize):
-    try:
-        bytes_request = sock.recv(buffersize)
-    except Exception:
-        connections.remove(sock)
-    else:
-        if bytes_request:
-            requests.append(bytes_request)
 
-
-def write(sock, connection, response):
-    try:
-        sock.send(response)
-    except Exception:
-        connection.remove(sock)
 
 
 parser = ArgumentParser()
@@ -49,8 +36,6 @@ if args.address:
 if args.port:
     default_config['port'] = args.port
 
-host, port = (default_config.get('host'), default_config.get('port'))
-
 logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s - %(levelname)s - %(message)s',
@@ -60,46 +45,12 @@ logging.basicConfig(
     ]
 )
 
-requests = []
-connections = []
+app = Application(
+    default_config.get('host'),
+    default_config.get('port'),
+    default_config.get('buffersize'),
+    handle_default_request
+)
 
-try:
-    sock = socket()
-    sock.bind((host, port,))
-    sock.settimeout(0)
-    sock.listen(5)
-
-    logging.info(f'Server was started with {host}:{port}')
-
-    while True:
-        try:
-            client, address = sock.accept()
-
-            connections.append(client)
-
-            logging.info(f'Client was connected with {address[0]}:{address[1]} | Connections: {connections}')
-        except Exception:
-            pass
-
-        rlist, wlist, xlist = select.select(
-            connections, connections, connections, 0
-        )
-
-        for r_client in rlist:
-            r_thread = threading.Thread(
-                target=read, args=(r_client, connections, requests, default_config.get('buffersize'))
-            )
-            r_thread.start()
-
-        if requests:
-            b_request = requests.pop()
-            b_response = handle_default_request(b_request)
-
-            for w_client in wlist:
-                w_thread = threading.Thread(
-                    target=write, args=(w_client, connections, b_response)
-                )
-                w_thread.start()
-
-except KeyboardInterrupt:
-    logging.info('Server shutdown')
+app.bind()
+app.run()
